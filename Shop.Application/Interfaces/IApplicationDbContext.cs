25526770816e1a01fd6e0988239723ba3db8ac0d
rkrawczyk98 <47090131+rkrawczyk98@users.noreply.github.com>
2023-05-13 18:23:51 +0200

    -do projektu .Application dodano interfejs IApplicationDbContext -do projeku .Domain dodano BaseEntity oraz Product - usunięto niepotrzebne pliki

---
+++ Shop.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Product> Products { get; set; }
+        Task<int> SaveChanges();
+    }
+}
---
+++ Shop.Domain/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string Category { get; set; }
+        public string CategoryName { get; set; }
+        public string? CategoryDescription { get; set; }
+        //public string CategoryCategory { get; set; }
+        public string Price { get; set; }
+        public string? AuthorId { get; set; }
+    }
+}
