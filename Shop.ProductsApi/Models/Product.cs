bed700f7b6b6d2ea21f8dc1980c5e389c8156b38
Konrad Dąbrowski <konrad.dabrowski@microsoft.wsei.edu.pl>
2023-05-22 18:56:19 +0200

    ProductsApi init

---
+++ Shop.ProductsApi/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop.ProductsApi.Models
+{
+    public class Product
+    {
+        public uint ProductId { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public Category? Category { get; set; }
+        public string Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
--- Shop.ProductsApi/Program.cs
+++ Shop.ProductsApi/Program.cs
@@ -1,11 +1,31 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.ProductsApi;
+using Shop.ProductsApi.Data;
+using Shop.ProductsApi.Models;
+//using Shop.UsersApi.Services;
+using System;
+using Shop.Application.Core.Services;
+using Shop.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => 
+{
+    options.UseSqlServer(connectionString);
+});
+
+/////////
+//builder.Services.AddScoped<IProductService, ProductService>();
+/////////
 
 var app = builder.Build();
 
